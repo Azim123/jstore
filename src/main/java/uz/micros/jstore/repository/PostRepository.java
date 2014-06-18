60b3c8d82fe1a3ccbec170fedc2db76ba8f253c0
Azim <azim@mail.ru>
2014-06-18 20:20:09 +0500

    Simple JPA integration

--- src/main/java/uz/micros/jstore/repository/PostRepository.java
+++ src/main/java/uz/micros/jstore/repository/PostRepository.java
@@ -3,6 +3,7 @@ package uz.micros.jstore.repository;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import uz.micros.jstore.entity.blog.Post;
 
@@ -14,32 +15,8 @@ import java.util.List;
 import java.util.Map;
 
 @Repository
-public class PostRepository {
+public interface PostRepository extends JpaRepository<Post, Integer> {
 
-    @Autowired
-    private SessionFactory sessionFactory;
 
-    public void addPost(Post post){
 
-    }
-
-    public Post getPost(int id){
-        return null;
-    }
-
-    public void savePost(Post post){
-
-    }
-
-    public void deletePost(int id){
-
-    }
-
-    @Transactional
-    public List<Post> getPosts() {
-
-        Session session = sessionFactory.getCurrentSession();
-
-        return session.createQuery("from Post").list();
-    }
 }
